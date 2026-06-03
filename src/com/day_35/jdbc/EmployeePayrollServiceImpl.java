37a26cdce3d2bd64f09e32431d4ec5fa8d423332
sathishn-coder <sathish.nethaji@bridgelabz.com>
2026-06-03 10:00:29 +0530

    Day 35: Implemented UC-12: Soft delete employee using is_active flag

---
+++ src/com/day_35/jdbc/EmployeePayrollServiceImpl.java
@@ -0,0 +1,25 @@
+package com.day_35.jdbc;
+
+
+
+public class EmployeePayrollServiceImpl {
+
+    public EmployeePayroll addEmployeePayroll(String name, double salary, String gender, boolean is_active) throws EmployeePayrollException {
+        EmployeePayroll employeePayroll = EmployeePayrollDBService.addEmployeeToDB(name, salary, gender, is_active);
+        return employeePayroll;
+    }
+
+    public EmployeePayroll addEmployeePayrollDetails(String name, double salary, String gender, boolean is_active) throws EmployeePayrollException {
+        EmployeePayroll employeePayroll = EmployeePayrollDBService.addEmployeeWithPayrollDetails(name, salary, gender, is_active);
+        return employeePayroll;
+    }
+
+    public boolean removeEmployee(int employeeId) throws EmployeePayrollException {
+        boolean isRemoved = EmployeePayrollDBService.removeEmployeeFromDB(employeeId);
+        if (isRemoved) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
