e05b69f78648426d34f0e3344fc11321d21f49eb
sathishn-coder <sathish.nethaji@bridgelabz.com>
2026-06-03 09:56:10 +0530

    Day 35: Implemented UC-12: Soft delete employee using is_active flag

---
+++ src/com/day_35/jdbc/EmployeePayroll.java
@@ -0,0 +1,83 @@
+package com.day_35.jdbc;
+
+
+
+import java.time.LocalDate;
+
+public class EmployeePayroll {
+    private int id;
+    private String name;
+    private double salary;
+    private String gender;
+    private LocalDate startDate;
+    private boolean isActive;
+
+    public EmployeePayroll(int id, String name, double salary, String gender, LocalDate startDate, boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.gender = gender;
+        this.startDate = startDate;
+        this.isActive = isActive;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeePayroll{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                ", gender='" + gender + '\'' +
+                ", startDate=" + startDate +
+                ", isActive=" + isActive +
+                '}';
+    }
+}
